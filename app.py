cab5d22d27a759ee475f2ec1a10ba4563582152e
Felipe Silveira <eng.fe.silveira@gmail.com>
2021-06-26 17:44:19 -0400

    FINALLY. Thanks for your bug heroku, thanks

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 from flask import Flask, jsonify, render_template, abort, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from sqlalchemy import Column, String, Integer
+from models import setup_db, Actor, Movie
 
 app = Flask(__name__, static_url_path='/static')
+setup_db(app)
 
 #  CORS
 #  ----------------------------------------------------------------
@@ -18,43 +19,6 @@ def after_request(response):
     header['Access-Control-Allow-Origin'] = '*'
     return response
 
-#  Db config
-#  ----------------------------------------------------------------
-
-database_path = os.environ['DATABASE_URL_HEROKU']
-app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-# db.init_app(app)
-
-#  Model
-#  ----------------------------------------------------------------
-
-class Actor(db.Model):
-    __tablename__ = 'actors'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    age = db.Column(db.String(120))
-    gender = db.Column(db.String(120))
-
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'age': self.age,
-            'gender': self.gender,
-        }
-
-db.app = app
-db.create_all()
 
 #  System
 #  ----------------------------------------------------------------
@@ -84,6 +48,15 @@ def get_actors():
         'error': False
     })
     
+@app.route('/movies')
+def get_movies():
+    selection = Movie.query.all()
+    movies = [item.serialize() for item in selection]
+
+    return jsonify({
+        'movies': movies,
+        'error': False
+    })
 
 if __name__ == '__main__':
     app.run()
