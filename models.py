425cc65385f8a715d9e1b3ce5d88d3b70d09ff25
Felipe Silveira <eng.fe.silveira@gmail.com>
2021-06-28 21:33:17 -0400

    fix: minor improvements

--- models.py
+++ models.py
@@ -27,7 +27,12 @@ def setup_db(app, database_path=database_path):
 #  ----------------------------------------------------------------
 
 def db_drop_and_create_all():
-    # db.drop_all() # heroku crashes -> activate for tests
+    '''
+    Please, remove the comment here in order to perform the tests
+    Heroku crashes if it is activated.
+    '''
+    # db.drop_all()
+    
     db.create_all()
 
     # add one demo row for actors
